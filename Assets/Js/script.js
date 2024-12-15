d1eebc947a3f73dbdcbd45aabb6e2d2d19f7d702
Portfolio Ready <139498700+Hopedevelopers254@users.noreply.github.com>
2024-12-15 02:57:35 -0800

    Update script.js

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -46,7 +46,7 @@ async function getBotResponse(prompt) {
   };
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAwPKz6B2XoMNZB0IdMHhSDc8JQ7aIN1hE",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyBm-wYyPX18m4srLj1wCTlVEyiVNWHBQ78",
     requestOptions
   );
 
